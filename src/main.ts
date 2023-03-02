f493444488c124980d712e44f4e99abdbffe280c
sc0ncept <sebconcept@outlook.com>
2023-03-02 21:51:02 +0100

    Simple cookie session added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
+import * as passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.use(
+    session({
+      name: 'NESTJS_SESSION_ID',
+      secret: 'QWERTYQWERTYQWERTY',
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 60000,
+      },
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
